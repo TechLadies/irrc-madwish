f8c05dd8a3da83176fc915c8e316b1bd1e56fb09
lauratsui <lauratsui93@gmail.com>
2020-10-03 15:27:06 +0800

    IRRC-38: added 4 status cards for student profiles

---
+++ frontend/src/components/statusCards/StatusCardDroppedOut.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="status-container">
+    <div class="card">
+      <div class="card-content">
+        <div class="card-status-details">
+          <div class="content">
+            <div class="content-title">STATUS</div>
+            <b-taglist>
+              <b-tag type="is-danger is-light" size="is-large"
+                >&#9679; Dropped Out</b-tag
+              >
+            </b-taglist>
+          </div>
+          <div class="content">
+            <div class="content-title">REASON</div>
+            <p> Lorem Ipsum </p>
+            </b-taglist>
+          </div>
+        </div>
+        <div class="content">
+          <div class="content-title">ACTION</div>
+          <div class="buttons">
+            <Button label="Reactivate for Matching" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "StatusCardUnmatched",
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+.status-container {
+  width: 448px;
+  height: 228px;
+}
+.status-container .card {
+  border-radius: 4px;
+}
+.card-content {
+  padding: 32px;
+}
+.card-status-details {
+  display: flex;
+  justify-content: space-between;
+}
+.card-status-details .content {
+  flex: 1;
+}
+.content p{
+  font-size: 16px;
+}
+.content {
+  font-size: 12px;
+}
+.content-title {
+  margin-bottom: 12px;
+}
+.buttons {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
