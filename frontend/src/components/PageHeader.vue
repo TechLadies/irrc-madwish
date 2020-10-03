82b690f79f1f639629df232032f26726a13f589b
aelbore <aelbore.sg@gmail.com>
2020-10-03 23:20:34 +0800

    use buefy sidebar, create reusable page, header and content

---
+++ frontend/src/components/PageHeader.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="header">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'PageHeader'
+}
+</script>
+
+<style>
+
+.header {
+  overflow: hidden;
+  background-color: #f1f1f1;
+  padding: 20px 10px;
+  width: 100%;
+  height: 10%;
+  border-bottom: 1px solid lightgray;
+}
+
+.header a {
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 12px;
+  text-decoration: none;
+  line-height: 25px;
+  border-radius: 4px;
+}
+
+.header a.logo {
+  font-size: 25px;
+  font-weight: bold;
+  cursor: default;
+}
+
+/* .header a:hover {
+  background-color: #ddd;
+  color: black;
+} */
+
+.header a.active {
+  background-color: dodgerblue;
+  color: white;
+}
+
+@media screen and (max-width: 500px) {
+  .header a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+}
+</style>
