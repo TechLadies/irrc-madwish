d69df9e6a612e4ec9eac4de54aa54dc3de38f582
xuanjane <68148934+xuanjane@users.noreply.github.com>
2020-10-10 15:19:26 +0800

    Unique constraint on [PhoneNumber, FirstName, LastName]

--- backend/migrations/20201009200757_students.js
+++ backend/migrations/20201009200757_students.js
@@ -4,9 +4,10 @@ const { tableStudents } = require('../src/models/student')
 exports.up = (knex) => {
   return knex.schema.createTable(tableStudents, (table) => {
     table.increments('StudentID').primary() 
-    table.text('PhoneNumber').notNullable().unique()  // ToDO Discuss how to ensure students are unique
+    table.text('PhoneNumber').notNullable()
     table.text('FirstName').notNullable()
     table.text('LastName').notNullable()
+    table.unique(['PhoneNumber', 'FirstName', 'LastName'])
     table.text('Source')
     table.integer('NativeLanguageID').references('NativeLanguageID').inTable('nativeLanguages').onDelete('CASCADE')
     table.enu('EnglishProficiency', [
