ea1b18f6828ea14b64471d2257887a7801c1032d
xuanjane <68148934+xuanjane@users.noreply.github.com>
2020-10-18 23:07:56 +0800

    PATCH method to update existing Student record

--- backend/src/helpers/students.js
+++ backend/src/helpers/students.js
@@ -1,4 +1,5 @@
 const db = require('../models/student')
+//const debug = require('debug')('app:students')
 
 exports.addStudent = async function (student) {
   try {
@@ -9,24 +10,16 @@ exports.addStudent = async function (student) {
   }
 }
 
-exports.updateNote = async function (note) {
+exports.patchStudent = async function (id, patchstudent) {
+  // debug("test update notes")
   try {
     const response = await db.Student.query()
-      .update(note)
-      .where()
+    // .patch(patchstudent)
+    // .findById(id)
+      .patchAndFetchById(id, patchstudent)
+      .throwIfNotFound()
     return response
   } catch (err) {
     return { err }
   }
 }
-
-// Reach goal (more time) (Make sure to create a new branch)
-// updateStudentStatus
-// updateUnmatchedToMatched
-
-// Commit 1
-// Update individual student fields
-
-// Commit 2
-// getPreviousStudentStatusID
-// getCurrentStudentStatusID
--- backend/src/routes/students.js
+++ backend/src/routes/students.js
@@ -6,11 +6,13 @@ const db = require('../models/index')
 const students = require('../helpers/students')
 
 const { UniqueViolationError } = require('objection')
+const { NotFoundError } = require('objection')
 
 /* GET students listing. */
 router.get('/', async (req, res) => {
-  //debug('Hello World!')
-  const students = await db.Student.query().select('FirstName', 'LastName')
+  // debug('Hello World!')
+
+  const students = await db.Student.query().select('FirstName', 'LastName', 'Notes')
   res.json(students)
 })
 
@@ -35,6 +37,36 @@ router.post('/', async (req, res) => {
       res.status(500).send({
         message: err.message,
         type: 'UnknownError',
+        data: {}
+      })
+    }
+
+    return
+  }
+
+  res.status(200).json(result)
+})
+
+/* PATCH student listing */
+// on Postman: http://localhost:3001/students/1
+router.patch('/:id', async (req, res) => {
+  // debug('Hello World!')
+  // console.log('hello world')
+  // console.log(req.body)
+  const result = await students.patchStudent(req.params.id, req.body)
+  // handle error
+  if (result.err) {
+    const err = result.err
+    if (err instanceof NotFoundError) {
+      res.status(409).send({
+        message: err.message,
+        type: 'StudentNotFound',
+        data: {}
+      })
+    } else {
+      res.status(500).send({
+        message: err.message,
+        type: 'UnknownError. Database contraints may be violated',
         data: {}
       })
     }
