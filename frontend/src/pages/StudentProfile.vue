2263b1e6080d93f83652cb857d7e726257b63ebb
lauratsui <lauratsui93@gmail.com>
2020-10-24 12:21:05 +0800

    included student history and profile details onto student page

--- frontend/src/pages/StudentProfile.vue
+++ frontend/src/pages/StudentProfile.vue
@@ -1,10 +1,17 @@
 <template>
   <Page blueBg>
     <div class="student-profile-container">
-      <section>Profile Details</section>
-      <div>
+      <StudentProfileCard />
+      <div class="student-profile-right">
         <section>Status</section>
-        <section>History</section>
+        <section class="student-history-section">
+          <StudentHistory
+            v-bind:items="[
+              { date: '2020-01-01', description: 'Joined MadWish', status: 1 },
+              { date: '2020-01-01', description: 'Joined MadWish', status: 3 },
+            ]"
+          />
+        </section>
       </div>
     </div>
   </Page>
@@ -12,11 +19,15 @@
 
 <script>
 import Page from "../components/Page.vue";
+import StudentProfileCard from "../components/StudentProfileCard.vue";
+import StudentHistory from "../components/StudentHistory/StudentHistory.vue";
 
 export default {
   name: "StudentProfile",
   components: {
     Page,
+    StudentProfileCard,
+    StudentHistory,
   },
 };
 </script>
@@ -24,5 +35,19 @@ export default {
 <style lang="scss">
 .student-profile-container {
   display: flex;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  padding-left: 14px;
+  min-height: calc(100vh - 48px);
+}
+.student-profile-right {
+  display: flex;
+  flex-direction: column;
+}
+.student-history-section {
+  padding-top: 24px;
+  padding-left: 24px;
+  display: flex;
+  flex: 1;
 }
 </style>
