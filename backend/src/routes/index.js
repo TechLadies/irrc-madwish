0bda462e3aa058b8d629c6e3dc34a5f381d00288
lauratsui <lauratsui93@gmail.com>
2020-10-31 15:20:28 +0800

    created new public folder for serving static assets later

--- backend/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-/* GET home page. */
-router.get('/', function (req, res) {
-  res.json({ message: 'Welcome to IRRC' })
-})
-
-module.exports = router
