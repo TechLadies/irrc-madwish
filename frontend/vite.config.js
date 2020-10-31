8357c8cf9db11369ef24f9fba93d54724ad10b5f
aelbore <aelbore.sg@gmail.com>
2020-10-31 15:10:05 +0800

    add proxy server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,13 @@
 import { createVuePlugin } from 'vite-plugin-vue2'
 
 export default {
+  proxy: {
+    '/api': {
+      target: 'http://jsonplaceholder.typicode.com',
+      changeOrigin: true,
+      rewrite: path => path.replace(/^\/api/, '')
+    }
+  },
   alias: {
     'vue': 'vue/dist/vue.esm.js'
   },
