8755d070b81efb6a940b474d8e78c55e69b9d9d7
xuanjane <68148934+xuanjane@users.noreply.github.com>
2020-10-31 15:52:14 +0800

    Skeleton for vuex store.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,12 @@
 
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import router from "./router.js"
 
+Vue.use(Vuex)
 Vue.use(Buefy)
 new Vue({ 
   router,
---
+++ frontend/src/store.js
@@ -0,0 +1,16 @@
+import Vuex from 'vuex'
+
+export default new Vuex.Store({
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  getters: {
+
+  }
+})
