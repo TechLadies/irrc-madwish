8395d3234a04f5ed9d4e700a7ec86325bf07d9d4
Laura Tsui <lauratsui93@gmail.com>
2020-11-06 20:59:20 +0800

    created new public folder for serving static assets later (#21)

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,8 +4,8 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
+const path = require('path')
 
-const indexRouter = require('./routes/index')
 const studentsRouter = require('./routes/students')
 const nativeLanguagesRouter = require('./routes/nativeLanguages')
 
@@ -17,8 +17,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-app.use('/', indexRouter)
-app.use('/students', studentsRouter)
-app.use('/nativeLanguages', nativeLanguagesRouter)
+app.use(express.static(path.resolve(__dirname, "../public")))
+
+// Backend routes
+app.use('/api/students', studentsRouter)
+app.use('/api/nativeLanguages', nativeLanguagesRouter)
+
+app.get("*", function(req, res) {
+  res.sendFile(path.resolve(__dirname, "../public/index.html"))
+})
 
 module.exports = app
