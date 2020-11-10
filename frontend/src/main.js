36ac625a630aee37b92a8d2c9f3f5fd6c3f14bc3
xuanjane <68148934+xuanjane@users.noreply.github.com>
2020-11-10 17:49:27 +0800

    Remove vuex call in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,11 @@
 
 import Vue from 'vue'
 import App from './App.vue'
-import Vuex from 'vuex'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import router from "./router.js"
 import store from "./store.js"
 
-Vue.use(Vuex)
 Vue.use(Buefy)
 new Vue({ 
   router,
