e1dba49441c61098c327d41895c8c5c2f01fa63d
aelbore <aelbore.sg@gmail.com>
2020-11-14 11:29:15 +0800

    revert the vite.config.js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,6 @@
 import { createVuePlugin } from 'vite-plugin-vue2'
 
 export default {
-  proxy: {
-    '/api': {
-      target: 'http://jsonplaceholder.typicode.com',
-      changeOrigin: true,
-      rewrite: path => path.replace(/^\/api/, '')
-    }
-  },
   alias: {
     'vue': 'vue/dist/vue.esm.js'
   },
