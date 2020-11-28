d31fddd202f28cc3d97ca9d0449b6ea157ddf0fb
lauratsui <lauratsui93@gmail.com>
2020-11-28 14:39:44 +0800

    modified backend to be able to receive NativeLanguageString and return NativeLanguageID

--- backend/src/helpers/students.js
+++ backend/src/helpers/students.js
@@ -1,6 +1,7 @@
-const db = require('../models/student')
+const db = require("../models/student");
 // const debug = require('debug')('app:students')
-const statuses = require('./statuses')
+const statuses = require("./statuses");
+const nativeLanguages = require("./nativeLanguages");
 
 const defaultOptions = {
   filters: {}
@@ -20,60 +21,79 @@ exports.getAllStudents = async function (options = defaultOptions) {
     const students = await query.select()
     return students
   } catch (err) {
-    return { err }
+    return { err };
   }
-}
+};
 
 exports.getStudentById = async function (id) {
   try {
     const student = await db.Student.query()
       .findById(id)
-      .withGraphFetched('[nativeLanguage, status, statusUpdates.nextStatus]')
-      .throwIfNotFound()
-    return student // return student[0] || 'Not found'
+      .withGraphFetched("[nativeLanguage, status, statusUpdates.nextStatus]")
+      .throwIfNotFound();
+    return student; // return student[0] || 'Not found'
   } catch (err) {
-    return { err }
+    return { err };
   }
-}
+};
 
 exports.addStudent = async function (student) {
   try {
-    const response = await db.Student.query().insert(student)
-    return response
+    const response = await db.Student.query().insert(student);
+    return response;
   } catch (err) {
-    return { err }
+    return { err };
   }
-}
+};
 
 exports.patchStudent = async function (id, patchStudent) {
   // debug("test update notes")
   try {
     const response = await db.Student.query()
       .patchAndFetchById(id, patchStudent)
-      .throwIfNotFound()
-    return response
+      .throwIfNotFound();
+    return response;
   } catch (err) {
-    return { err }
+    return { err };
   }
-}
+};
 
 exports.getStatusByStudentId = async function (studentID) {
   try {
     // This is the only executed query in this example.
-    const status = await db.Student.relatedQuery('status')
-      .for(studentID)
-    return status
+    const status = await db.Student.relatedQuery("status").for(studentID);
+    return status;
   } catch (err) {
-    return { err }
+    return { err };
   }
-}
+};
 
 exports.getStatusPromise = async function (statusString) {
   if (statusString != null) {
     // If request contains StatusString, return corresponding StatusID
-    return statuses.getStatusByStatusString(statusString)
+    return statuses.getStatusByStatusString(statusString);
   } else {
     // If StatusID and StatusString are both not provided, default to 'SCREENING' StatusID
-    return statuses.getStatusByStatusString('SCREENING')
+    return statuses.getStatusByStatusString("SCREENING");
   }
-}
+};
+exports.getNativeLanguagePromise = async function (nativeLanguageString) {
+  if (nativeLanguageString != null) {
+    const nativeLanguageResponse = await nativeLanguages.getNativeLanguageByString(
+      nativeLanguageString
+    );
+    if (nativeLanguageResponse && !nativeLanguageResponse.err) {
+      return nativeLanguageResponse;
+    } else if (!nativeLanguageResponse) {
+      const newNativeLanguage = await nativeLanguages.addNativeLanguage({
+        NativeLanguage: nativeLanguageString,
+      });
+      return newNativeLanguage;
+    } else {
+      throw nativeLanguageResponse.err;
+    }
+  } else {
+    // If NativeLanguage is empty, will throw an error
+    throw new Error("NativeLanguageString or NativeLanguageID is required");
+  }
+};
