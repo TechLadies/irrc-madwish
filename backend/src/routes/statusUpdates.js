b47959826bb8ca5f943ad8daad61b6dd2de6842b
lauratsui <lauratsui93@gmail.com>
2020-12-05 12:36:23 +0800

    added additional validations to status updates in backend

--- backend/src/routes/statusUpdates.js
+++ backend/src/routes/statusUpdates.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const statusUpdates = require('../helpers/statusUpdates')
 const statuses = require('../helpers/statuses')
+const students = require('../helpers/students')
 
 const { UniqueViolationError } = require('objection')
 
@@ -15,6 +16,30 @@ router.post('/', async (req, res) => {
   delete req.body.PrevStatusString
   delete req.body.NextStatusString
 
+  if (prevStatus.StatusID == nextStatus.StatusID) {
+    return res.status(422).send({
+      message: 'Previous Status and Next Status should not be the same.',
+      type: 'StatusError',
+      data: {
+        previousStatus: prevStatus.StatusID,
+        nextStatus: nextStatus.StatusID
+      }
+    })
+  }
+
+  const student = await students.getStudentById(req.body.StudentID)
+
+  if (student.StatusID !== prevStatus.StatusID) {
+    return res.status(409).send({
+      message: 'Previous status must match current student status.',
+      type: 'StatusMismatch',
+      data: {
+        submittedStatus: prevStatus.StatusID,
+        actualStatus: student.StatusID
+      }
+    })
+  }
+  
   const result = await statusUpdates.addStatusUpdate(req.body)
 
   // handle error
