60cd9394984a0cbe1b9947686ac64e9470312ad4
dzldebby <debby.lingzl@gmail.com>
2020-12-19 13:45:52 +0800

    marged master

--- backend/src/routes/students.js
+++ backend/src/routes/students.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 // const debug = require('debug')('app:students')
-const students = require("../helpers/students");
+const students = require('../helpers/students')
 
-const { UniqueViolationError } = require("objection");
-const { NotFoundError } = require("objection");
+const { UniqueViolationError } = require('objection')
+const { NotFoundError } = require('objection')
 
 /* GET students listing. */
 router.get('/', async (req, res) => {
@@ -18,184 +18,182 @@ router.get('/', async (req, res) => {
   // handle error
   if (result.err) {
     // console.log('entered result.err')
-    const err = result.err;
+    const err = result.err
     res.status(500).send({
       message: err.message,
-      type: "UnknownError",
-      data: {},
-    });
-    return;
+      type: 'UnknownError',
+      data: {}
+    })
+    return
   }
 
-  res.json(result);
-});
+  res.json(result)
+})
 
 /* GET student by ID */
-router.get("/:id", async (req, res) => {
-  const result = await students.getStudentById(req.params.id);
+router.get('/:id', async (req, res) => {
+  const result = await students.getStudentById(req.params.id)
   //  console.log(result.err)
 
   // handle error
   if (result.err) {
-    const err = result.err;
+    const err = result.err
     if (err instanceof NotFoundError) {
       res.status(409).send({
         message: err.message,
-        type: "StudentNotFound",
-        data: {},
-      });
+        type: 'StudentNotFound',
+        data: {}
+      })
     } else {
       res.status(500).send({
         message: err.message,
-        type: "UnknownError",
-        data: {},
-      });
+        type: 'UnknownError',
+        data: {}
+      })
     }
 
-    return;
+    return
   }
 
-  res.json(result);
-});
+  res.json(result)
+})
 
 /* POST students listing */
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   // If request does not contain StatusID
   if (req.body.StatusID == null) {
-    var statusString;
+    var statusString
     // If request contains a statusString
     if (req.body.StatusString != null) {
-      statusString = req.body.StatusString;
-      delete req.body.StatusString;
+      statusString = req.body.StatusString
+      delete req.body.StatusString
     }
-    const status = await students.getStatusPromise(statusString);
-    req.body.StatusID = status.StatusID;
+    const status = await students.getStatusPromise(statusString)
+    req.body.StatusID = status.StatusID
   }
 
   // If request does not contain NativeLanguageID
   try {
     if (req.body.NativeLanguageID == null) {
-      let nativeLanguageString;
+      let nativeLanguageString
       // If request contains a nativeLanguageString
       if (req.body.NativeLanguageString != null) {
-        nativeLanguageString = req.body.NativeLanguageString;
-        delete req.body.NativeLanguageString;
+        nativeLanguageString = req.body.NativeLanguageString
+        delete req.body.NativeLanguageString
       }
       const nativeLanguage = await students.getNativeLanguagePromise(
         nativeLanguageString
-      );
-      req.body.NativeLanguageID = nativeLanguage.NativeLanguageID;
+      )
+      req.body.NativeLanguageID = nativeLanguage.NativeLanguageID
     }
   } catch (err) {
     res.status(400).send({
       message: err.message,
-      type: "MissingParams",
-      data: {},
-    });
+      type: 'MissingParams',
+      data: {}
+    })
   }
 
-  const result = await students.addStudent(req.body);
+  const result = await students.addStudent(req.body)
 
   // handle error
   if (result.err) {
-    const err = result.err;
+    const err = result.err
     if (err instanceof UniqueViolationError) {
       res.status(409).send({
         message: err.message,
-        type: "UniqueViolation",
+        type: 'UniqueViolation',
         data: {
           columns: err.columns,
           table: err.table,
-          constraint: err.constraint,
-        },
-      });
+          constraint: err.constraint
+        }
+      })
     } else {
       res.status(500).send({
         message: err.message,
-        type: "UnknownError",
-        data: {},
-      });
+        type: 'UnknownError',
+        data: {}
+      })
     }
 
-    return;
+    return
   }
 
-  res.status(200).json(result);
-});
+  res.status(200).json(result)
+})
 
 /* PATCH student listing */
 // on Postman: http://localhost:3001/students/1
-router.patch("/:id", async (req, res) => {
+router.patch('/:id', async (req, res) => {
   if (req.body.NativeLanguageString) {
     try {
       const nativeLanguage = await students.getNativeLanguagePromise(
         req.body.NativeLanguageString
-      );
-      req.body.NativeLanguageID = nativeLanguage.NativeLanguageID;
-      delete req.body.NativeLanguageString;
-    }
-    catch (err) {
+      )
+      req.body.NativeLanguageID = nativeLanguage.NativeLanguageID
+      delete req.body.NativeLanguageString
+    } catch (err) {
       return res.status(500).send({
         message: err.message,
-        type: "UnknownError with NativeLanguage",
-        data: {},
-      });
+        type: 'UnknownError with NativeLanguage',
+        data: {}
+      })
     }
   }
-  
-  
-  const result = await students.patchStudent(req.params.id, req.body);
+
+  const result = await students.patchStudent(req.params.id, req.body)
   // handle error
   if (result.err) {
-    const err = result.err;
+    const err = result.err
     if (err instanceof NotFoundError) {
       res.status(409).send({
         message: err.message,
-        type: "StudentNotFound",
-        data: {},
-      });
+        type: 'StudentNotFound',
+        data: {}
+      })
     } else {
       res.status(500).send({
         message: err.message,
-        type: "UnknownError. Database contraints may be violated",
-        data: {},
-      });
+        type: 'UnknownError. Database contraints may be violated',
+        data: {}
+      })
     }
 
-    return;
+    return
   }
 
-  res.status(200).json(result);
-});
+  res.status(200).json(result)
+})
 
 /* GET status by StudentID */
-router.get("/:id/status", async (req, res) => {
-  const result = await students.getStatusByStudentId(req.params.id);
+router.get('/:id/status', async (req, res) => {
+  const result = await students.getStatusByStudentId(req.params.id)
   // console.log(result.err)
 
   // if student not found
   if (result.length === 0) {
     res.status(409).send({
-      message: "StudentNotFound",
-      type: "StudentNotFound",
-      data: {},
-    });
-    return;
+      message: 'StudentNotFound',
+      type: 'StudentNotFound',
+      data: {}
+    })
+    return
   }
 
   // handle error
   if (result.err) {
     // console.log('entered result.err')
-    const err = result.err;
+    const err = result.err
     res.status(500).send({
       message: err.message,
-      type: "UnknownError",
-      data: {},
-    });
-    return;
+      type: 'UnknownError',
+      data: {}
+    })
+    return
   }
 
-  res.json(result);
-});
+  res.json(result)
+})
 
-module.exports = router;
+module.exports = router
