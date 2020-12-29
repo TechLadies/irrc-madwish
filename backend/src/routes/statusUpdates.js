1649c5e920829fb67c229f4b272bf211d7278d85
Laura Tsui <lauratsui93@gmail.com>
2020-12-29 13:34:46 +0800

    [irrc-71] validations to status updates in backend (#48)
    * added additional validations to status updates in backend
    
    * Apply suggestions from code review
    
    commiting changes suggested by Yingtong
    
    Co-authored-by: ying tong <yingtong@z.cash>
    
    Co-authored-by: ying tong <yingtong@z.cash>

--- backend/src/routes/statusUpdates.js
+++ backend/src/routes/statusUpdates.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const statusUpdates = require('../helpers/statusUpdates')
 const statuses = require('../helpers/statuses')
+const students = require('../helpers/students')
 
 const { UniqueViolationError } = require('objection')
 
@@ -20,6 +21,30 @@ router.post('/', async (req, res) => {
     delete req.body.NextStatusString
   }
 
+  if (req.body.PreviousStatusID == req.body.NextStatusID) {
+    return res.status(422).send({
+      message: 'Previous Status and Next Status should not be the same.',
+      type: 'StatusError',
+      data: {
+        previousStatus: req.body.PreviousStatusID,
+        nextStatus: req.body.NextStatusID
+      }
+    })
+  }
+
+  const student = await students.getStudentById(req.body.StudentID)
+
+  if (student.StatusID !== req.body.PreviousStatusID) {
+    return res.status(409).send({
+      message: 'Previous status must match current student status.',
+      type: 'StatusMismatch',
+      data: {
+        submittedStatus: req.body.PreviousStatusID,
+        actualStatus: student.StatusID
+      }
+    })
+  }
+  
   const result = await statusUpdates.addStatusUpdate(req.body)
 
   // handle error
