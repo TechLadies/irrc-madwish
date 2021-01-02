50555c5de6b340c732e8d2e075132d0f6b51ed8e
Laura Tsui <lauratsui93@gmail.com>
2021-01-02 11:52:50 +0800

    created new teacher Vuex store for refactoring (#61)
    Co-authored-by: Thomas Chia <thomas.h.chia@googlemail.com>

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,32 +1,54 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import {nativeLanguageActions, nativeLanguageGetters, nativeLanguageMutations, nativeLanguageState} from './store/nativeLanguages.js'
-import {studentActions, studentGetters, studentMutations, studentState} from './store/students.js'
-import {screeningActions, screeningState, screeningMutations} from './store/screening.js'
+import Vue from "vue";
+import Vuex from "vuex";
+import {
+  nativeLanguageActions,
+  nativeLanguageGetters,
+  nativeLanguageMutations,
+  nativeLanguageState,
+} from "./store/nativeLanguages.js";
+import {
+  teacherActions,
+  teacherMutations,
+  teacherGetters,
+  teacherState,
+} from "./store/teachers.js";
+import {
+  screeningActions,
+  screeningState,
+  screeningMutations
+} from "./store/screening.js";
+import {
+  studentActions,
+  studentGetters,
+  studentMutations,
+  studentState
+} from "./store/Students.js";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 // Create enum style variables (to reduce typo errors)
 const MUTATIONS = Object.freeze({
-  SET_STUDENTS: 'SET_STUDENTS'
-})
+  SET_STUDENTS: "SET_STUDENTS",
+});
 
 export default new Vuex.Store({
   state: {
     ...studentState,
     ...nativeLanguageState,
+    ...teacherState,
     ...screeningState
   },
   mutations: {
     ...studentMutations,
     ...nativeLanguageMutations,
+    ...teacherMutations,
     ...screeningMutations
   },
   actions: {
     ...studentActions,
     ...nativeLanguageActions,
+    ...teacherActions,
     ...screeningActions,
-
     // Update student status
     async updateStudentStatus({ commit, dispatch }, { studentID, previousStatusString, nextStatusString, updatedBy }) {
       // PATCH student
@@ -107,7 +129,8 @@ export default new Vuex.Store({
   getters: {
     ...studentGetters,
     ...nativeLanguageGetters,
+    ...teacherGetters,
     screeningStudents: (state) => state.students.filter((student) => student.status.Description === "SCREENING"),
     getStudentByStudentId: (state) => (id) => state.students.find(student => student.StudentID == id)
   },
-})
+});
---
+++ frontend/src/store/teachers.js
@@ -0,0 +1,37 @@
+import Vuex from "vuex";
+
+const MUTATIONS = Object.freeze({
+  SET_TEACHERS: "SET_TEACHERS",
+});
+export const teacherState = { teachers: [] };
+export const teacherActions = {
+  async getAllTeachers({ commit }) {
+    const response = await fetch("/api/teachers");
+    const teacherData = await response.json();
+    commit(MUTATIONS.SET_TEACHERS, teacherData);
+  },
+  async createTeacher({ dispatch }, teacherData) {
+    const payload = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify(teacherData),
+    };
+    const response = await fetch("api/teachers", payload);
+    dispatch("getAllTeachers");
+    return response;
+  },
+};
+export const teacherMutations = {
+  [MUTATIONS.SET_TEACHERS](state, teacherData) {
+    state.teachers = teacherData;
+  },
+};
+export const teacherGetters = {
+  teachers(state) {
+    return state.teachers;
+  },
+};
