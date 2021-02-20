d7292961c6e182665d843d65bfe36955606c6ab3
ying tong <yingtong@z.cash>
2021-02-20 10:12:58 +0800

    Remove ADD CONSTRAINT from down migration

--- backend/migrations/20210214110931_teachersProficiency.js
+++ backend/migrations/20210214110931_teachersProficiency.js
@@ -12,10 +12,6 @@ exports.up = function(knex, Promise) {
   exports.down = async function(knex) {
     return knex.raw(`
         ALTER TABLE "teachers" DROP CONSTRAINT "teachers_LanguageProficiency_check";
-        ALTER TABLE "teachers" ADD CONSTRAINT "teachers_LanguageProficiency_check"
-        CHECK ("LanguageProficiency" IN ('Little', 'Simple', 'Intermediate', 'Fluent'));
         ALTER TABLE "teachers" DROP CONSTRAINT "teachers_EnglishProficiency_check";
-        ALTER TABLE "teachers" ADD CONSTRAINT "teachers_EnglishProficiency_check"
-        CHECK ("EnglishProficiency" IN ('Little', 'Simple', 'Intermediate', 'Fluent'));
     `);
   };
