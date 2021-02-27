d7e8be1b147998a34d6bb11f3785859b9713f131
Tomoe <suzukitomoe.st@gmail.com>
2021-02-27 12:09:36 +0800

    Upon a change in teacher's status to Unmatched/Dropped Out from the teacher profile, all students linked to the teacher have matches deleted and the students' statuses changed to Unmatched

--- backend/src/routes/matches.js
+++ backend/src/routes/matches.js
@@ -2,18 +2,18 @@ const express = require('express')
 const router = express.Router()
 const db = require('../models/index')
 const statuses = require('../helpers/statuses')
+const statusUpdates = require('../helpers/statusUpdates.js')
+
 
 /* GET matches listing. */
 router.get('/', async (req, res) => {
-    const matches = await db.Match.query().select('MatchID', 'StudentID', 'TeacherID', 'StudentStatusUpdateID', 'TeacherStatusUpdateID', 'LastEmailDate', 'MatchStatus', 'isDeleted', 'ConfirmedDate')
+    const matches = await db.Match.query().select('MatchID', 'StudentID', 'TeacherID', 'StudentStatusUpdateID', 'TeacherStatusUpdateID', 'LastEmailDate', 'MatchStatus', 'ConfirmedDate')
     res.json(matches)
   })
 
-
-/* POST Delete matches from teacher profile by changing isDeleted to true */
+/* POST Delete matches from teacher profile */
 const deleteMatches = async ({field, id, nextStatusString, res})=>{ 
-    const statusDeleteMatch = ["UNMATCHED", "DROPPED OUT"]
-    // TODO: Query and collect all matches for a given student/teacher 
+  // Check if id or nextStatusString are null/undefined/empty
     if (id == null || id == undefined) {
       return res.status(500).send({
         message: `${field} is undefined or null`,
@@ -21,11 +21,36 @@ const deleteMatches = async ({field, id, nextStatusString, res})=>{
       })
     }
     if (nextStatusString == "" || nextStatusString == null || nextStatusString == undefined) {
+      // console.log(nextStatusString)
       return res.status(500).send({
         message: `nextStatusString is empty, undefined or null.`,
         type: 'UnknownError'
       })
     }
+/* Check if field is TeacherID */ 
+    if (field === "TeacherID") {
+      // Query for studentIDs linked to the TeacherID
+      const studentsMatched = await db.Match.query().select("StudentID").where(field, id)
+      // Update status of all students linked to the TeacherID 
+      // Asynchronously get current status in any order 
+      const [currentStatus, nextStatus] = await Promise.all([
+        statuses.getStatusByStatusString('MATCHED'),
+        statuses.getStatusByStatusString('UNMATCHED')
+      ])
+      // TODO: Implement status updates for students as a transaction to avoid cases where status is changed only for some 
+      await Promise.all(studentsMatched.map(async student => {
+        // Construct statusUpdate
+        const statusUpdate = {
+          StudentID: student.StudentID,
+          PreviousStatusID: currentStatus.StatusID,
+          NextStatusID: nextStatus.StatusID,
+          UpdatedBy: "IRRCAdmin"
+        }
+        await statusUpdates.addStatusUpdate(statusUpdate)
+      }))
+    }
+    const statusDeleteMatch = ["UNMATCHED", "DROPPED OUT"]
+    
     const nextStatus = await statuses.getStatusByStatusString(nextStatusString)
     if (statusDeleteMatch.includes(nextStatus.Description)){
       await db.Match.query()
