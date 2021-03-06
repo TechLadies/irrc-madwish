067f6fc9e4e6d37e287b8638d7d70f834c270dc4
therealyingtong <yingtong@electriccoin.co>
2021-03-07 02:07:18 +0800

    Move getStatusPromise() into helpers/statuses

--- backend/src/helpers/statuses.js
+++ backend/src/helpers/statuses.js
@@ -1,11 +1,24 @@
-const db = require('../models/status')
+const db = require("../models/status");
 
 exports.getStatusByStatusString = async function (statusString) {
   try {
-    const response = await db.Status.query()
-      .findOne('Description', 'ilike', statusString)
-    return response
+    const response = await db.Status.query().findOne(
+      "Description",
+      "ilike",
+      statusString
+    );
+    return response;
   } catch (err) {
-    return { err }
+    return { err };
   }
-}
+};
+
+exports.getStatusPromise = async function (statusString) {
+  if (statusString != null) {
+    // If request contains StatusString, return corresponding StatusID
+    return statuses.getStatusByStatusString(statusString);
+  } else {
+    // If StatusID and StatusString are both not provided, default to 'SCREENING' StatusID
+    return statuses.getStatusByStatusString("SCREENING");
+  }
+};
--- backend/src/routes/teachers.js
+++ backend/src/routes/teachers.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+const statuses = require("../helpers/statuses");
 const students = require("../helpers/students");
 const teachers = require("../helpers/teachers");
 
@@ -41,7 +42,7 @@ router.post("/", async (req, res) => {
       statusString = req.body.StatusString;
       delete req.body.StatusString;
     }
-    const status = await students.getStatusPromise(statusString);
+    const status = await statuses.getStatusPromise(statusString);
     req.body.StatusID = status.StatusID;
   }
 
@@ -118,22 +119,20 @@ router.post("/", async (req, res) => {
   res.status(200).json(result);
 });
 
-
-router.patch('/:id', async (req, res) => {
+router.patch("/:id", async (req, res) => {
   if (req.body.NativeLanguageString) {
     try {
       const nativeLanguage = await students.getNativeLanguagePromise(
         req.body.NativeLanguageString
-      )
-      req.body.NativeLanguageID = nativeLanguage.NativeLanguageID
-      delete req.body.NativeLanguageString
-    
+      );
+      req.body.NativeLanguageID = nativeLanguage.NativeLanguageID;
+      delete req.body.NativeLanguageString;
     } catch (err) {
       return res.status(500).send({
         message: err.message,
-        type: 'UnknownError with NativeLanguage',
-        data: {}
-      })
+        type: "UnknownError with NativeLanguage",
+        data: {},
+      });
     }
   }
 
@@ -141,42 +140,40 @@ router.patch('/:id', async (req, res) => {
     try {
       const secondLanguage = await students.getNativeLanguagePromise(
         req.body.SecondLanguageString
-      )
-      req.body.SecondLanguageID = secondLanguage.NativeLanguageID
-      delete req.body.SecondLanguageString
-    
+      );
+      req.body.SecondLanguageID = secondLanguage.NativeLanguageID;
+      delete req.body.SecondLanguageString;
     } catch (err) {
       return res.status(500).send({
         message: err.message,
-        type: 'UnknownError with SecondLanguage',
-        data: {}
-      })
+        type: "UnknownError with SecondLanguage",
+        data: {},
+      });
     }
   }
 
-  const result = await teachers.patchTeacher(req.params.id, req.body)
+  const result = await teachers.patchTeacher(req.params.id, req.body);
 
   // handle error
   if (result.err) {
-    const err = result.err
+    const err = result.err;
     if (err instanceof NotFoundError) {
       res.status(409).send({
         message: err.message,
-        type: 'TeacherNotFound',
-        data: {}
-      })
+        type: "TeacherNotFound",
+        data: {},
+      });
     } else {
       res.status(500).send({
         message: err.message,
-        type: 'UnknownError. Database contraints may be violated',
-        data: {}
-      })
+        type: "UnknownError. Database contraints may be violated",
+        data: {},
+      });
     }
 
-    return
+    return;
   }
   res.status(200).json(result);
 });
-
 
 module.exports = router;
