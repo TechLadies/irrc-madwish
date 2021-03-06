9c212d67847676facb36197c3e455845bb62dc38
Laura Tsui <lauratsui93@gmail.com>
2021-03-06 14:28:38 +0800

    recommitting, including suggestedTeachers logic, formatting (#102)

--- frontend/src/pages/Teachers.vue
+++ frontend/src/pages/Teachers.vue
@@ -1,163 +1,191 @@
 <template>
-    <Page>
-        <PageHeader slot="header">
-        <a class="logo">Teachers</a>
-       
-        </PageHeader>   
-        
-        <section>
-            <b-table
-            :data="teachersData"
-            :selected.sync="selected"
-            @dblclick="goToTeacher"
-            >
-                <b-table-column field="created_at" label="Date Joined" width="120" searchable sortable>
-                     <template
-                    slot="searchable"
-                    slot-scope="props"
-                    >
-                    <b-tooltip label="Search: YYYY-MM-DD">
-                        <b-input
-                        v-model="props.filters[props.column.field]"
-                        icon="magnify"
-                        size="is-small"
-                        />
-                    </b-tooltip>
-                    
-                    </template>
-                    <template v-slot="props">
-                        
-                        <span style="font-size: 14px">
-                        {{
-                            new Date(props.row.created_at)
-                            .toLocaleDateString("en-US", {
-                                day: "2-digit",
-                                month: "short",
-                                year: "numeric",
-                            })
-                            .replace(",", " ")
-                        }}
-                        </span>
-                    </template>
-                </b-table-column>
+  <Page>
+    <PageHeader slot="header">
+      <a class="logo">Teachers</a>
+    </PageHeader>
 
-                <b-table-column field="FullNameID" label="Teacher Name/ID" searchable sortable>
-                     <template
-                    slot="searchable"
-                    slot-scope="props"
-                    >
-                    <b-input
-                    v-model="props.filters[props.column.field]"
-                    icon="magnify"
-                    size="is-small"
-                    />
-                    </template>
-                    <template v-slot="props">
-                        <span class="name">
-                            {{props.row.FullName}}
-                        </span>
-                        <br>
-                        <span class="ID">
-                            {{props.row.TeacherID}}
-                        </span>
-                   </template> 
-                </b-table-column>
+    <section>
+      <b-table
+        :data="teachersData"
+        :selected.sync="selected"
+        @dblclick="goToTeacher"
+      >
+        <b-table-column
+          field="created_at"
+          label="Date Joined"
+          width="120"
+          searchable
+          sortable
+        >
+          <template slot="searchable" slot-scope="props">
+            <b-tooltip label="Search: YYYY-MM-DD">
+              <b-input
+                v-model="props.filters[props.column.field]"
+                icon="magnify"
+                size="is-small"
+              />
+            </b-tooltip>
+          </template>
+          <template v-slot="props">
+            <span style="font-size: 14px">
+              {{
+                new Date(props.row.created_at)
+                  .toLocaleDateString("en-US", {
+                    day: "2-digit",
+                    month: "short",
+                    year: "numeric",
+                  })
+                  .replace(",", " ")
+              }}
+            </span>
+          </template>
+        </b-table-column>
+      </b-table>
+    </section>
+    <section>
+      <b-table :data="teachersData">
+        <b-table-column
+          field="created_at"
+          label="Date Joined"
+          width="120"
+          searchable
+          sortable
+        >
+          <template slot="searchable" slot-scope="props">
+            <b-tooltip label="Search: YYYY-MM-DD">
+              <b-input
+                v-model="props.filters[props.column.field]"
+                icon="magnify"
+                size="is-small"
+              />
+            </b-tooltip>
+          </template>
+          <template v-slot="props">
+            <span style="font-size: 14px">
+              {{
+                new Date(props.row.created_at)
+                  .toLocaleDateString("en-US", {
+                    day: "2-digit",
+                    month: "short",
+                    year: "numeric",
+                  })
+                  .replace(",", " ")
+              }}
+            </span>
+          </template>
+        </b-table-column>
 
-                <b-table-column field="status" label="Status" searchable sortable>
-                    <template
-                    slot="searchable"
-                    slot-scope="props"
-                    >
-                    <b-input
-                    v-model="props.filters[props.column.field]"
-                    icon="magnify"
-                    size="is-small"
-                    />
-                    </template>
-                    <template v-slot="props">
-                        <Status :status="props.row.status.Description"></Status>
-                    </template>
-                  
-                </b-table-column>
+        <b-table-column
+          field="FullNameID"
+          label="Teacher Name/ID"
+          searchable
+          sortable
+        >
+          <template slot="searchable" slot-scope="props">
+            <b-input
+              v-model="props.filters[props.column.field]"
+              icon="magnify"
+              size="is-small"
+            />
+          </template>
+          <template v-slot="props">
+            <span class="name">
+              {{ props.row.FullName }}
+            </span>
+            <br />
+            <span class="ID">
+              {{ props.row.TeacherID }}
+            </span>
+          </template>
+        </b-table-column>
 
-                <b-table-column field="Email" label="Email" searchable sortable>
-                    <template
-                    slot="searchable"
-                    slot-scope="props"
-                    >
-                    <b-input
-                    v-model="props.filters[props.column.field]"
-                    icon="magnify"
-                    size="is-small"
-                    />
-                    </template>
-                    <template v-slot="props">
-                        {{ props.row.Email }}
-                    </template>
-                    
-                </b-table-column>
+        <b-table-column field="status" label="Status" searchable sortable>
+          <template slot="searchable" slot-scope="props">
+            <b-input
+              v-model="props.filters[props.column.field]"
+              icon="magnify"
+              size="is-small"
+            />
+          </template>
+          <template v-slot="props">
+            <Status :status="props.row.status.Description"></Status>
+          </template>
+        </b-table-column>
 
-                <b-table-column field="PhoneNumber" label="Phone Number" searchable sortable>
-                    <template
-                    slot="searchable"
-                    slot-scope="props"
-                    >
-                    <b-input
-                    v-model="props.filters[props.column.field]"
-                    icon="magnify"
-                    size="is-small"
-                    />
-                    </template>
-                    <template v-slot="props">
-                        {{ props.row.PhoneNumber }}
-                    </template>
-                 </b-table-column>
-            </b-table>
-        </section> 
+        <b-table-column field="Email" label="Email" searchable sortable>
+          <template slot="searchable" slot-scope="props">
+            <b-input
+              v-model="props.filters[props.column.field]"
+              icon="magnify"
+              size="is-small"
+            />
+          </template>
+          <template v-slot="props">
+            {{ props.row.Email }}
+          </template>
+        </b-table-column>
 
-    </Page>
+        <b-table-column
+          field="PhoneNumber"
+          label="Phone Number"
+          searchable
+          sortable
+        >
+          <template slot="searchable" slot-scope="props">
+            <b-input
+              v-model="props.filters[props.column.field]"
+              icon="magnify"
+              size="is-small"
+            />
+          </template>
+          <template v-slot="props">
+            {{ props.row.PhoneNumber }}
+          </template>
+        </b-table-column>
+      </b-table>
+    </section>
+  </Page>
 </template>
 
 <script>
-import Page from "../components/Page.vue"
-import PageHeader from "../components/PageHeader.vue"
-import Status from "../components/Status.vue"
-import { mapGetters, mapActions } from "vuex"
+import Page from "../components/Page.vue";
+import PageHeader from "../components/PageHeader.vue";
+import Status from "../components/Status.vue";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
-    name: "AllTeachers",
-    components: {
-        Page,
-        PageHeader,
-        Status
-    },
-    computed: {
-        teachersData() {
-            return this.teachers.map(item => {
-                return {
-                    ...item, 
-                    FullNameID: `${item.FullName} <br> ${item.TeacherID}`,
-                }
-            })
-        },
-        ...mapGetters(['teachers'])
-    },
-    data() {
+  name: "AllTeachers",
+  components: {
+    Page,
+    PageHeader,
+    Status,
+  },
+  computed: {
+    teachersData() {
+      return this.teachers.map((item) => {
         return {
-          selected: {},
-        }
+          ...item,
+          FullNameID: `${item.FullName} <br> ${item.TeacherID}`,
+        };
+      });
+    },
+    ...mapGetters(["teachers", "suggestedTeachers"]),
+    data() {
+      return {
+        selected: {},
+      };
     },
     methods: {
-        ...mapActions(['getAllTeachers']),
-        goToTeacher() {
-          this.$router.push({ path: `/teachers/${this.selected.TeacherID}` })
-        }
+      ...mapActions(["getAllTeachers"]),
+      goToTeacher() {
+        this.$router.push({ path: `/teachers/${this.selected.TeacherID}` });
+      },
     },
     mounted() {
-        this.getAllTeachers()
-    }
-    
-}
+      this.getAllTeachers();
+    },
+  },
+};
 </script>
 
 <style lang="scss">
@@ -201,7 +229,7 @@ span.tag.is-warning {
 }
 span.ID {
   font-size: 14px;
-  color: #59666e
+  color: #59666e;
 }
 .table td,
 .table th {
@@ -209,7 +237,7 @@ span.ID {
 }
 span.name {
   color: #3c4f76 !important;
-  font-size: 16px
+  font-size: 16px;
 }
 
 table td:not([align]),
