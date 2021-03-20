df227d9e54f39aaec68222c7fa90aa796a36341c
therealyingtong <yingtong@z.cash>
2021-03-20 18:00:16 +0800

    Default student FirstName to FullName when rolling back migration

--- backend/migrations/20201107134701_remove_firstName_lastName_from_students.js
+++ backend/migrations/20201107134701_remove_firstName_lastName_from_students.js
@@ -1,17 +1,20 @@
-const { tableStudents } = require('../src/models/student')
+const { tableStudents } = require("../src/models/student");
 
-exports.up = function(knex, Promise) {
-    return knex.schema.table(tableStudents, (table) => {
-        table.dropUnique(['PhoneNumber', 'FirstName', 'LastName']);
-        table.dropColumn('FirstName');
-        table.dropColumn('LastName');
-    });
+exports.up = function (knex, Promise) {
+  return knex.schema.table(tableStudents, (table) => {
+    table.dropUnique(["PhoneNumber", "FirstName", "LastName"]);
+    table.dropColumn("FirstName");
+    table.dropColumn("LastName");
+  });
 };
 
-exports.down = function(knex, Promise) {
-    return knex.schema.table(tableStudents, (table) => {
-        table.text('FirstName').notNullable().defaultTo('')
-        table.text('LastName').notNullable().defaultTo('')
-        table.unique(['PhoneNumber', 'FirstName', 'LastName'])
-    });
+exports.down = async function (knex, Promise) {
+  await knex.schema.table(tableStudents, (table) => {
+    table.text("FirstName").notNullable().defaultTo("");
+    table.text("LastName").notNullable().defaultTo("");
+  });
+  await knex.raw(`UPDATE "students" SET "FirstName" = "FullName"`);
+  await knex.schema.table(tableStudents, (table) => {
+    table.unique(["PhoneNumber", "FirstName", "LastName"]);
+  });
 };
