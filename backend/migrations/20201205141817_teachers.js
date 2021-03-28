7e8500b5ee9c5b81e2f7d1eb63418ba797531490
Tomoe <suzukitomoe.st@gmail.com>
2021-03-28 15:52:08 +0800

    added TeachingExperience as enum yes/no in migrations teachers.js

--- backend/migrations/20201205141817_teachers.js
+++ backend/migrations/20201205141817_teachers.js
@@ -1,34 +1,41 @@
-const { tableTeachers } = require('../src/models/teacher')
-
+const { tableTeachers } = require("../src/models/teacher");
 
 exports.up = (knex) => {
   return knex.schema.createTable(tableTeachers, (table) => {
-    table.increments('TeacherID').primary() 
-    table.text('PhoneNumber').notNullable()
-    table.text('Email')
-    table.text('FullName').notNullable()
-    table.unique(['PhoneNumber', 'FullName'])
-    table.text('Source')
-    table.integer('NativeLanguageID').references('NativeLanguageID').inTable('nativeLanguages').onDelete('CASCADE')
-    table.integer('SecondLanguageID').references('NativeLanguageID').inTable('nativeLanguages').onDelete('CASCADE')
-    table.enu('EnglishProficiency', [
+    table.increments("TeacherID").primary();
+    table.text("PhoneNumber").notNullable();
+    table.text("Email");
+    table.text("FullName").notNullable();
+    table.unique(["PhoneNumber", "FullName"]);
+    table.text("Source");
+    table
+      .integer("NativeLanguageID")
+      .references("NativeLanguageID")
+      .inTable("nativeLanguages")
+      .onDelete("CASCADE");
+    table
+      .integer("SecondLanguageID")
+      .references("NativeLanguageID")
+      .inTable("nativeLanguages")
+      .onDelete("CASCADE");
+    table.enu("EnglishProficiency", ["No", "Little", "Simple", "Intermediate"]);
+    table.enu("LanguageProficiency", [
       "No",
       "Little",
       "Simple",
-      "Intermediate"
-    ])
-    table.enu('LanguageProficiency', [
-        "No",
-        "Little",
-        "Simple",
-        "Intermediate"
-      ])
-    table.text('Notes')
-    table.integer('StatusID').references('StatusID').inTable('statuses').onDelete("CASCADE")
-    table.timestamps(true, true)
-  })
-}
+      "Intermediate",
+    ]);
+    table.enu("TeachingExperience", ["Yes", "No"]);
+    table.text("Notes");
+    table
+      .integer("StatusID")
+      .references("StatusID")
+      .inTable("statuses")
+      .onDelete("CASCADE");
+    table.timestamps(true, true);
+  });
+};
 
 exports.down = (knex) => {
-  return knex.schema.dropTable(tableTeachers)
-}
+  return knex.schema.dropTable(tableTeachers);
+};
